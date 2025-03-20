ee27a2aa349f14a5ba7faa85cf462b7f2485402c
Victor Mota <41929297+Victor-MFr@users.noreply.github.com>
2025-03-20 14:29:16 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,14 @@
+import java.sql.Date;
+
+public class Pessoa {
+    private static int pessoaId;
+    private Conta contaId;
+    private String nome;
+    private String email;
+    private String CPF;
+    private Date dataNascimento;
+
+    public void Cadastro(){
+
+    }
+}
