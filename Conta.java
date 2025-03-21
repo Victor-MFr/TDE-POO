3631d2b8262d99f66d59c65c1ee19ebcd9c887a7
Victor Mota <41929297+Victor-MFr@users.noreply.github.com>
2025-03-21 16:37:59 -0300

    Add files via upload

--- Conta.java
+++ Conta.java
@@ -1,4 +1,23 @@
 public class Conta {
-    private int contaId;
+    private static int contaId = 0;
+    private double saldo = 0;
+    private double valorDeposito = 0;
+    private double valorSaque = 0;
+    private double valorTransferencia = 0;
+    private double valorSimulacao = 0;
+
+    public void Depositar() {
+        this.saldo += valorDeposito;
+    }
     
+    public void Sacar() {
+    }
+
+    public void Transferir() {
+
+    }
+
+    public void SimuladorDeRendimento() {
+
+    }
 }
